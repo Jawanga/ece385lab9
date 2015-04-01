dfe45341e39c548265148e32791cd82984c9f1be
Jawanga <jason.tao.wang@gmail.com>
2015-04-01 06:55:09 -0500

    some off by one error in RotWord or SubWord

--- lab9software/main.c
+++ lab9software/main.c
@@ -7,12 +7,37 @@ int main() {
 	int Nb = 4;
 	int Nr = 10;
 	unsigned char key[16] = {0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F};
+	//unsigned char key[16] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
 	unsigned char decrypted_message[16] = { 0xEC, 0xE2, 0x98, 0xDC, 0xEC, 0xE2, 0x98, 0xDC, 0xEC, 0xE2, 0x98, 0xDC, 0xEC, 0xE2, 0x98, 0xDC };
 	unsigned char encrypted_message[16];
 	unsigned long key_expansion[44];
 	KeyExpansion(key, key_expansion, Nk);
 	int j;
-	for (j = 40; j < 44; j++) {
+	for (j = 0; j < 4; j++) {
+		printf("%x", key_expansion[j]);
+	}
+	printf("\n");
+	for (j = 4; j < 8; j++) {
+		printf("%x", key_expansion[j]);
+	}
+	printf("\n");
+	for (j = 8; j < 12; j++) {
+		printf("%x", key_expansion[j]);
+	}
+	printf("\n");
+	for (j = 12; j < 16; j++) {
+		printf("%x", key_expansion[j]);
+	}
+	printf("\n");
+	for (j = 16; j < 20; j++) {
+		printf("%x", key_expansion[j]);
+	}
+	printf("\n");
+	for (j = 20; j < 24; j++) {
+		printf("%x", key_expansion[j]);
+	}
+	printf("\n");
+	for (j = 24; j < 28; j++) {
 		printf("%x", key_expansion[j]);
 	}
 	printf("\n");
